03272d1a8168b196e7ffe6b194d0c02a5ed9c627
Berserker2412 <103943937+Berserker2412@users.noreply.github.com>
2024-04-04 08:46:59 -0500

    Add files via upload

---
+++ Java/main (1).java
@@ -0,0 +1,10 @@
+public class main{
+  public static void main(String[] args) {
+    Pasajero pasajero1= new Pasajero ("Juliana", "Pacheco", "JR-123", "A1", 19);
+    Pasajero pasajero2= new Pasajero ("Juan", "Perez", "JR-456", "A2", 20);
+
+    pasajero1.MenorEdad(pasajero1, pasajero2);
+  }
+
+  
+}
